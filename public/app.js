e22980044b2948c7e69c5473fe020d003bca1011
Paul Salaets <psalaets@gmail.com>
2015-08-16 01:21:07 -0400

    pattern input directive with keyboard helpers

--- public/app.js
+++ public/app.js
@@ -2,19 +2,10 @@ angular.module('gsv', [])
 .controller('MainCtrl', function($scope, clientMessaging) {
 
   $scope.files = [];
-  $scope.patterns = [{
-    value: ''
-  }];
 
-  $scope.$watch('patterns', function(newValue, oldValue) {
-    if (newValue !== oldValue) {
-      var patterns = newValue.map(function(patternObject) {
-        return patternObject.value;
-      });
-
-      clientMessaging.sendPatterns(patterns);
-    }
-  }, 'deep');
+  $scope.sendPatterns = function(patterns) {
+    clientMessaging.sendPatterns(patterns);
+  };
 
   clientMessaging.onAllFiles(function(files) {
     files.sort(byPath);
@@ -42,6 +33,84 @@ angular.module('gsv', [])
   function byPath(file1, file2) {
     return file1.path.localeCompare(file2.path);
   }
+})
+.directive('patternInputs', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      onPatternChange: '&'
+    },
+    controller: function($scope) {
+      $scope.patterns = [newPattern()];
+      focusInput(0);
+
+      $scope.$watch('patterns', function(newValue, oldValue) {
+        if (newValue !== oldValue) {
+          var patterns = newValue.map(function(patternObject) {
+            return patternObject.value;
+          });
+
+          $scope.onPatternChange({patterns: patterns});
+        }
+      }, 'deep');
+
+      $scope.handleKey = function(event, index) {
+        var handler = {
+          '8': handleBackspaceKey,
+          '13': handleEnterKey
+        }[event.keyCode];
+
+        handler && handler(index);
+      };
+
+      function handleEnterKey(index) {
+        addPattern(index);
+        focusInput(index + 1);
+      }
+
+      function handleBackspaceKey(index) {
+        if (patternCount() > 1 && hasEmptyPatternAt(index)) {
+          removePattern(index);
+          focusInput(index - 1);
+        }
+      }
+
+      function focusInput(index) {
+        if (index < 0) {
+          index = 0;
+        }
+
+        // HackCity 3000
+        setTimeout(function() {
+          document.getElementById('pattern-' + index).focus();
+        }, 0)
+      }
+
+      function hasEmptyPatternAt(index) {
+        return $scope.patterns[index].value.length == 0;
+      }
+
+      function addPattern(afterIndex) {
+        $scope.patterns.splice(afterIndex + 1, 0, newPattern());
+      }
+
+      function newPattern() {
+        return {
+          value: ''
+        };
+      }
+
+      function patternCount() {
+        return $scope.patterns.length;
+      }
+
+      function removePattern(index) {
+        $scope.patterns.splice(index, 1);
+      }
+    },
+    // template is embedded in index.html
+    templateUrl: 'pattern-inputs-script-tag'
+  };
 })
 .factory('clientMessaging', function($rootScope) {
   var allFilesListener, selectedFilesListener;
