5d42368523aabab9ea646a671d9cee97eba268e2
Paul Salaets <psalaets@gmail.com>
2015-08-29 11:05:21 -0400

    run autoprefixer on css for #1

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,26 @@
 var gulp = require('gulp');
 var gsv = require('./');
 
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+
 // for testing gulp-src-viewer
 gulp.task('default', function() {
   gsv(gulp);
+});
+
+gulp.task('build', function() {
+  return gulp.src('public/css/app.css')
+    .pipe(autoprefixer({
+      browsers: [
+        'last 2 versions',
+        'Chrome >= 31',
+        'Firefox >= 38',
+        'Opera >= 30',
+        'Safari >= 7.1',
+        'Explorer >= 10'
+      ]
+    }))
+    .pipe(rename('app.good.css'))
+    .pipe(gulp.dest('public/css'))
 });
